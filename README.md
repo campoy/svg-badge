b6bd40b25456655fd782fc4ee9281156e80916dc
Francesc Campoy <campoy@golang.org>
2018-01-10 15:44:56 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # svg-badge
-Code for svg-badge.appspot.com
+
+Have you ever needed to create SVG badges for your GitHub repositories?
+
+Now everything you need to do is use SVG badge!
+
+Using a url like `https://svg-badge.appspot.com/badge/kind/label?color=aabbcc`
+you will obtain an SVG image with the text `kind` followed by `label` on a
+background of color `#aabbcc`.
+
+Try it!
+
+The image below use `stability`, `stable`, and color `3a3`
+
+[![stable](https://svg-badge.appspot.com/badge/stability/stable?color=3a3)](https://svg-badge.appspot.com/badge/stability/stable?color=3a3)
+
+The application runs on Google App Engine.
---
+++ main.go
@@ -0,0 +1,64 @@
+package app
+
+import (
+	"net/http"
+
+	svg "github.com/ajstarks/svgo"
+	"github.com/gorilla/mux"
+)
+
+func init() {
+	r := mux.NewRouter()
+	r.HandleFunc("/badge/{kind}/{label}", badgeHandler)
+	http.Handle("/", r)
+}
+
+var colors = map[string]string{
+	"deprecated":   "C62914",
+	"experimental": "DD5F0A",
+	"frozen":       "4b4b4b",
+	"locked":       "14C6C6",
+	"stable":       "74C614",
+}
+
+func badgeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "image/svg+xml")
+	w.Header().Set("Cache-Control", "max-age=31556926")
+
+	vars := mux.Vars(r)
+	kind := vars["kind"]
+	label := vars["label"]
+	color := colors[label]
+	if v := r.FormValue("color"); v != "" {
+		color = v
+	}
+
+	const (
+		height    = 20
+		shadow    = 2
+		padding   = 10
+		charWidth = 8
+	)
+
+	kindWidth := charWidth * len(kind)
+	labelWidth := charWidth * len(label)
+	width := 4*padding + kindWidth + labelWidth
+
+	s := svg.New(w)
+	s.Start(width+shadow, height+shadow)
+	s.Style("text/css", `
+		text {
+			alignment-baseline: middle;
+			font-family: Courier, monospace;
+			font-size: 12px;
+			fill: white;
+			text-shadow: 1px 1px #666
+		}`)
+
+	s.Rect(shadow, shadow, width, height, "fill:#aaa")
+	s.Rect(0, 0, 2*padding+kindWidth, height, "fill:#333")
+	s.Rect(2*padding+kindWidth, 0, 2*padding+labelWidth, height, "fill:#"+color)
+	s.Text(padding, height/2, kind)
+	s.Text(3*padding+kindWidth, height/2, label)
+	s.End()
+}
